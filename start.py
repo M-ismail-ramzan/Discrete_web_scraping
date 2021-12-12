2d4428d1ffc5bf1942155117c21f63fd012c8af7
M-ismail-ramzan <m.ismail_ramzan@yahoo.com>
2021-12-12 21:20:40 +0500

    finally

--- start.py
+++ start.py
@@ -9,7 +9,7 @@ from website1 import *
 #print (first_website_total[0])
 from matplotlib import pyplot
 import numpy as np
-
+import matplotlib.pyplot as plt
 # first_website_total,second_website_total & third_website_total
 # 0 index contains the total number of Nouns
 # 1 index contains total number of Verbs
@@ -36,29 +36,72 @@ print(first_website_max_nouns_list)
 print(first_website_max_adjective_list)
 print(first_website_max_verbs_list)
 
-first_website_top_10_nouns = []
-top_10_noun_extrator(first_website_max_nouns_list,first_website_top_10_nouns)
-print("TOP 10 Nouns of First Website")
-print(first_website_top_10_nouns)
+
+def show_final_website_graph(website_text,max_nouns_in_website):
+    
+    other_list_=[]
+    # making the list of the top apperance
+    for each in max_nouns_in_website:
+        other_list_.append(each[0])
+    G = nx.Graph()
+    text = str(website_text)
+    text=TextBlob(text)
+    
+    #Looping for Each Sentence!
+    for sentence in text.sentences:
+        #Check all the Nouns in the Sentence
+        nouns = [n for n, t in text.tags if (
+            t == 'NN' or t == 'NNS' or t == 'NNP' or t == 'NNPS')]
+        #Check for each of the noun
+        for index, elem in enumerate(nouns):
+            #Getting all the Nouns next & Previous so we can add them
+            if (index+1 < len(nouns) and index - 1 >= 0) and (len(elem) > 4):
+                #prev_el = str(nouns[index-1])
+                if len(nouns[index+1]) > 4 and nouns[index+1] in other_list_:
+                    curr_el = str(elem)
+                    next_el = str(nouns[index+1])
+                    #Making the nodes of th e
+                    G.add_node(curr_el)
+                    G.add_node(next_el)
+                    #Now adding those Nodes together
+                    G.add_edge(curr_el,next_el)
+        #print(sentence)
 
 
-print(first_website_text)
+    pos=nx.spring_layout(G, k=0.99 ,iterations=20,scale=2)
+    nx.draw(G,pos,with_labels=True,node_color='skyblue',font_size = 4)
+    print("Number of Connected Componets is:" + str(nx.number_connected_components(G)))
+    plt.show()
+
+show_final_website_graph(first_website_text,first_website_max_nouns_list)
 """
 print("THE MAXIMUM OCCURANCES OF THE second WEBSITE")
 print(second_website_max_nouns_list)
 print(second_website_max_adjective_list)
 print(second_website_max_verbs_list)
 
+comparison_graph_display(first_website_total[0],second_website_total[0],third_website_total[0],"Nouns")
+comparison_graph_display(first_website_total[1],second_website_total[1],third_website_total[1],"Verbs")
+comparison_graph_display(first_website_total[2],second_website_total[2],third_website_total[2],"Adjective")
 
-second_website_top_10_nouns = []
-top_10_noun_extrator(second_website_max_nouns_list,second_website_top_10_nouns)
-print("TOP 10 Nouns of Second Website")
-print(second_website_top_10_nouns)
 
 print("THE MAXIMUM OCCURANCES OF THE third WEBSITE")
 print(third_website_max_nouns_list)
 print(third_website_max_adjective_list)
 print(third_website_max_verbs_list)
+
+
+
+first_website_top_10_nouns = []
+top_10_noun_extrator(first_website_max_nouns_list,first_website_top_10_nouns)
+print("TOP 10 Nouns of First Website")
+print(first_website_top_10_nouns)
+
+
+second_website_top_10_nouns = []
+top_10_noun_extrator(second_website_max_nouns_list,second_website_top_10_nouns)
+print("TOP 10 Nouns of Second Website")
+print(second_website_top_10_nouns)
 
 third_website_top_10_nouns = []
 top_10_noun_extrator(third_website_max_nouns_list,third_website_top_10_nouns)
@@ -80,7 +123,5 @@ def comparison_graph_display(first_pof,second_pof,third_pof,labels):
 # 0 index contains the total number of Nouns
 # 1 index contains total number of Verbs
 # 2 Index contains total number of Ajection
-comparison_graph_display(first_website_total[0],second_website_total[0],third_website_total[0],"Nouns")
-comparison_graph_display(first_website_total[1],second_website_total[1],third_website_total[1],"Verbs")
-comparison_graph_display(first_website_total[2],second_website_total[2],third_website_total[2],"Adjective")
+
 """
--- wow.py
+++ wow.py
@@ -4,7 +4,8 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import nxviz as nv
 
-text = [b"FAST National University " ]
+text = [b" Campus in 2009. Currently, He is working as a Senior IT Auditor, KPMG Dublin, Ireland. Previously he worked for Deloitte Islamabad as an Assistant Manager IT Audit. Admissions for FALL 2020 are open! To apply please follow the link: http://pwr.nu.edu.pk/admissions/ Last date to apply: 30th July 2020 Muhammad Batch 10 (BSCS) Hi, I am Farwa Zaki. CS graduate of Batch 12. Currently, I am working as a Data Analyst in Telenor Pakistan. My message for @FASTians is, The best"]
+
 first_website_top_10_nouns = [['courses', 111], ['research', 74], ['student', 59], ['PhD', 57], ['program', 56], ['students', 46], ['degree', 39], ['knowledge', 39], ['credit', 35], ['management', 32]]
 
 other_list = [['courses', 111], ['research', 74], ['student', 59], ['PhD', 57], ['program', 56], ['students', 46], ['degree', 39], ['knowledge', 39], ['credit', 35], ['management', 32], ['research', 13], ['ORIC', 12], ['Campus', 11], ['List', 10], ['FAST', 10], ['Date', 10], ['Home', 9], ['Faculty', 9], ['PhD', 9], ['Islamabad', 9], ['Karachi', 10], ['students', 9], ['Campus', 8], ['University', 6], ['Computer', 6], ['FAST', 6], ['event', 6], ['National', 5], ['Sciences', 5], ['skills', 5], ['Engineering', 13], ['FAST', 10], ['Lahore', 9], ['Department', 9], ['School', 7], ['Campus', 6], ['Electrical', 6], ['Civil', 6], ['Science', 6], ['campus', 6], ['Peshawar', 25], ['Campus', 22], ['FAST', 19], ['Computer', 15], ['University', 14], 
@@ -18,17 +19,10 @@ for each in other_list:
 
 # Now i have the top 10 Nouns of the website let's create it's graph
 G = nx.Graph()
-# Adding nodes to the graph
-for each in first_website_top_10_nouns:
-    G.add_node(each[0])
-#Now finding the adjacent element in each Node!
 
 
-G.add_edge('PHD', 'program')
 
-#plt.show()
-
-text = str(text)
+text=str(text)
 text=TextBlob(text)
 #Looping for Each Sentence!
 test_list = []
@@ -57,5 +51,6 @@ for sentence in text.sentences:
 
 pos=nx.spring_layout(G, k=0.99 ,iterations=20,scale=2)
 nx.draw(G,pos,with_labels=True,node_color='skyblue',font_size = 4)
-
+print(nx.number_connected_components(G))
 plt.show()
+
